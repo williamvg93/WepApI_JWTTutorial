8a0083502e280fddbc6fb7605125ec83cd2af183
williamvg93 <sonico_5071@hotmail.com>
2025-02-13 19:26:52 -0500

    feat: :sparkles: The entities and dbContext were created using DBfirst(Scaffold-DbContext)

---
+++ ApiJWT/Models/ProductosJwtContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiJWT.Models;
+
+public partial class ProductosJwtContext : DbContext
+{
+    public ProductosJwtContext()
+    {
+    }
+
+    public ProductosJwtContext(DbContextOptions<ProductosJwtContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=productosJWT;Integrated Security=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__products__3214EC078574231F");
+
+            entity.ToTable("products");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__users__3214EC078352AE36");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
