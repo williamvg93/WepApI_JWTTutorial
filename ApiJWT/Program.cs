68f54e1fa2b2b3ecc4a03e32ea43c9a35ae0f82b
williamvg93 <sonico_5071@hotmail.com>
2025-02-14 22:55:05 -0500

    feat: :sparkles: The utilities Class and the JWT Configuration were added to the Dependency Control

--- ApiJWT/Program.cs
+++ ApiJWT/Program.cs
@@ -1,41 +1,64 @@
+using System.Text;
+using ApiJWT.Custom;
+using ApiJWT.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+/* builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); */
+
+
+builder.Services.AddDbContext<ProductosJwtContext>(opt => {
+    string conecString = builder.Configuration.GetConnectionString("SqlServerCon")!;
+    opt.UseSqlServer(conecString); 
+});
+
+// Agregando como dependencias las funciones de la clase utilidades
+builder.Services.AddSingleton<Utilities>();
+
+// Agregando como dependencias el jwt y sus configuraciones
+builder.Services.AddAuthentication(conf => {
+    conf.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    conf.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config => {
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+        IssuerSigningKey = new SymmetricSecurityKey
+        (Encoding.UTF8.GetBytes(builder.Configuration["JWT:key"]!))
+    };
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    /* app.UseSwagger(); */
+    /* app.UseSwaggerUI(); */
     app.MapOpenApi();
+    app.UseSwaggerUi(opt => {
+        opt.Path = "/openapi";
+        opt.DocumentPath = "/openapi/v1.json";
+    });
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Para que funcione la authenticacion con JWT
+app.UseAuthentication();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
